62f44330ff7c8a55e8b4e8d512b2f3c8c6c4baa1
Valeriia <43146435+ValeriiaHolovatiuk@users.noreply.github.com>
2026-05-29 00:40:09 +0200

    feat: added styles for scoreboard

--- src/components/Scores.jsx
+++ src/components/Scores.jsx
@@ -1,8 +1,14 @@
 export function Scores({ currentScore, bestScore }) {
     return (
         <div id="scores-wrapper">
-            <p>Current Score: {currentScore}</p>
-            <p>Best Score: {bestScore}</p>
+            <div id="current-score-wrapper" className="scores">
+                <p className="score-label label">Current Score</p>
+                <p className="score-value">{currentScore}</p>
+            </div>
+            <div id="best-score-wrapper" className="scores">
+                <p className="score-label label">Best Score</p>
+                <p className="score-value">{bestScore}</p>
+            </div>
         </div>
     )
 }
