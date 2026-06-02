25f9e19f9e624376ca92721c84ef7980c5a9cf46
Valeriia <43146435+ValeriiaHolovatiuk@users.noreply.github.com>
2026-06-02 22:51:13 +0200

    feat: continue working on styling
    started styling of the card container and cards with images themselves

--- src/components/DifficultySelector.jsx
+++ src/components/DifficultySelector.jsx
@@ -1,7 +1,7 @@
 export function DifficultySelector() {
     return (
         <div id="difficulty-selector-wrapper">
-            <button className="select-btn easy" id="difficulty-easy-btn">Easy</button>
+            <button className="select-btn easy selected" id="difficulty-easy-btn">Easy</button>
             <button className="select-btn medium" id="difficulty-medium-btn">Medium</button>
             <button className="select-btn hard" id="difficulty-hard-btn">Hard</button>
         </div>
